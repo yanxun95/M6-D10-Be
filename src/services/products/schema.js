bcaefdb31c7e994b312fb2747b87406fe5d616be
kolluvinay425 <kolluvinay425@gmail.com>
2021-10-08 15:26:23 +0200

    update

--- src/services/products/schema.js
+++ src/services/products/schema.js
@@ -10,22 +10,9 @@ const productSchema = new Schema(
     imageUrl: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String },
-    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+    reviews: [],
   },
   { timestamps: true }
 );
-productSchema.static("findReviews", async function (mongoQuery) {
-  // this cannot be an arrow function
-  // const total = await this.countDocuments(mongoQuery.criteria)
-  const reviews = await this.find(
-    mongoQuery.criteria,
-    mongoQuery.options.fields
-  )
-    //   .limit(mongoQuery.options.limit || 10)
-    //   .skip(mongoQuery.options.skip)
-    //   .sort(mongoQuery.options.sort) // no matter how I write them but Mongo will always apply SORT then SKIP then LIMIT in this order
-    .populate({ path: "reviews" }); // this is goin' to "join" authors with books by searching for all the references in the authors array (array of objectIds)
 
-  return { total, books };
-});
 export default model("Product", productSchema);
