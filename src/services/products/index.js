779e2109ec3051c99d69c58ba1e804adc2e5e39b
BananaKun <yanxun951224@gmail.com>
2021-10-08 10:39:02 +0100

    init

---
+++ src/services/products/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import createHttpError from "http-errors"
+import ProductModel from "./schema.js"
+import ReviewModel from "../reviews/schema.js"
+import mongoose from "mongoose"
+
+const productRouter = express.Router()
+
+productRouter.post("/", async (req, res, next) => {
+    try {
+        const newProduct = new ProductModel(req.body)
+        const { _id } = await newProduct.save()
+
+        res.status(201).send({ _id })
+    } catch (error) {
+        next(error)
+    }
+})
+
+productRouter.get("/", async (req, res, next) => {
+    try {
+        const products = await ProductModel.find()
+
+        res.send(blogs)
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default productRouter
---
+++ src/services/reviews/schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const reviewSchema = new Schema(
+    {
+        comment: { type: String, required: true },
+        rate: { type: Number, required: true },
+        productId: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+export default model("Review", reviewSchema)
